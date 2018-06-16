5916244e7d01c07121b1e8cec003eb1a6fb1c5c8
Ricardo Gomez <rgomez@gatech.edu>
2018-06-16 12:46:19 -0600

    Moved file into src

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,14 +1,18 @@
-const blankLine = '  |   |  '
-const guessLine = '1 |   |  '
-const bombLine = '  | B |  '
+const printBoard = (board) => {
+    console.log('Current board: ');
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+};
 
-console.log("This is what an empty board would look like: ");
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+let board = [
+[' ', ' ', ' '],
+[' ', ' ', ' '],
+[' ', ' ', ' ']
+];
 
-console.log(" This is what a board with a guess and a bomb on it would look like: ");
+printBoard(board);
+board[0][1] =  '1';
+board[2][2] = 'B';
+printBoard(board);
 
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
